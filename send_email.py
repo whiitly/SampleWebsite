47c6d4b7d740a8e445d3e9d9c74a2783f5757353
whitney <whitney22391@gmail.com>
2022-09-22 17:51:44 +0800

    changed receiver

--- send_email.py
+++ send_email.py
@@ -1,13 +1,14 @@
 import smtplib, ssl
 import os
 
-
 port = 465
 smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
+RECEIVER = "t0924148@u.nus.edu"
+
 message = """\
-Suject: Github Email Report
+Suject: Github Action Email Report
 
 This is your daily email report.
 """
@@ -15,5 +16,7 @@ This is your daily email report.
 context = ssl.create.default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(USERNAME, PASSWORD)
-    server.sendmail(USERNAME, USERNAME, message)
-    
+    # server.sendmail(USERNAME, USERNAME, message)
+    server.sendmail(USERNAME, RECEIVER, message)
+    server.quit()
+    print("Successfully sent email")
